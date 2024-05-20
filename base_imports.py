5672b25fac4271efeb38404db866fb69d2804fde
orhanakpinar <orhan.akpinar1@gmail.com>
2024-05-20 15:51:53 +0300

    Updating
    Updating 1 missing package and false import request

--- base_imports.py
+++ base_imports.py
@@ -8,7 +8,8 @@ import re
 from sklearn.metrics import f1_score, accuracy_score
 import spacy
 from spacy.lang.tr.stop_words import STOP_WORDS as tr_stop_words
-from sklearn.feature_extraction.text import CountVectorizer, train_test_split, KFold
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split, KFold
 from gensim import corpora, models
 import gensim
 from gensim.models import CoherenceModel
